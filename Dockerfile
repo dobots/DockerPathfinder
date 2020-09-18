928dd6e8a282b8e97fc7afc58b49569b1bcfecb2
Ludo Stellingwerff <ludo@almende.org>
2020-09-18 10:50:38 +0200

    Updates to be able to run Vibhav's code

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,9 @@ ENV NVIDIA_DRIVER_CAPABILITIES \
 
 RUN git clone https://github.com/osrf/gzweb.git
 RUN curl -sL https://deb.nodesource.com/setup_10.x | bash -
-RUN apt install -y nodejs libjansson-dev libboost-dev imagemagick libtinyxml-dev mercurial
+RUN apt-get update
+RUN apt-get install -y nodejs libjansson-dev libboost-dev imagemagick libtinyxml-dev mercurial
+RUN apt-get install -y ros-melodic-navigation ros-melodic-hector-slam ros-melodic-teb-local-planner
 WORKDIR /gzweb
 RUN  . /usr/share/gazebo/setup.sh; ./deploy.sh -m
 EXPOSE 8080
--- run_rosgazebo.sh
+++ run_rosgazebo.sh
@@ -1,3 +1,29 @@
+#!/bin/bash
+
+
+#check if there is a /build and /devel in the current PWD
+if [ -f devel/.catkin ]; then
+echo "Starting from Catkin Workspace: $PWD, making it available in '/projects' in the container."
+
+sed -i 's/;\/projects\/src//' devel/.catkin
+echo -n "`cat devel/.catkin`;/projects/src" > devel/.catkin
+
+docker run -iPt \
+    --rm \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="$XAUTHORITY:$XAUTHORITY" \
+    --env="XAUTHORITY=$XAUTHORITY" \
+    --volume="$PWD:/projects" \
+    --runtime=nvidia \
+    --name="gazebo_test" \
+    gazebo_test:latest \
+    bash
+
+else
+echo "If you run this script from a built catkin workspace it will make that workspace available to the Docker container."
+
 docker run -iPt \
     --rm \
     --env="DISPLAY=$DISPLAY" \
@@ -9,4 +35,7 @@ docker run -iPt \
     --name="gazebo_test" \
     gazebo_test:latest \
     bash
+fi
+
+
 
